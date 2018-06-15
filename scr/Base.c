347a54295e7ecccd6752d4412b1cea4ee80b55bb
GEORGE0051 <36436394+GEORGE0051@users.noreply.github.com>
2018-06-15 14:34:24 +0700

    Changed from Cyrillic to English

--- scr/Base.c
+++ scr/Base.c
@@ -4,13 +4,17 @@
 int getStickN(int playern, int stickcount) {
     int n ;
     while (1) {
-        printf("\n\n\n\n		Èãðîê N%d, ââåäèòå ÷èñëî ñïè÷åê, êîòîðîå âû áåðåòå:>",playern) ;
+        printf("\n\n\n\n		Player N%d, enter the number of matches you take:>",playern) ;
         fflush(stdin) ;
         scanf("%d",&n) ;
         fflush(stdin) ;
 
-        if (n>stickcount) printf("		Íåëüçÿ âçÿòü áîëüøå, ÷åì ñåé÷àñ íà ñòîëå!\n") ; else
-        if ((n<1)||(n>10)) printf("		Íóæíî ââåñòè îò 1 äî 10!\n") ; else
+        if (n>stickcount) 
+            printf("		You can't have more than that on the table.!\n") ;
+        else
+        if ((n<1)||(n>10)) 
+            printf("		You need to enter from 1 to 10!\n") ;
+        else
         return n ;
     }
 }
@@ -19,29 +23,31 @@ void goGame() {
     int stickcount = 100 ;
     int playern = 1 ;
     while (stickcount>0) {
-       printf("\n\n\n\n								Òåêóùåå ÷èñëî ñïè÷åê: %d\n",stickcount) ;
+       printf("\n\n\n\n							    Current number of matches: %d\n",stickcount) ;
        int n = getStickN(playern,stickcount);
        stickcount-=n ;
-       if (playern==1) playern=2 ; else playern=1 ;
+       if (playern==1) playern=2 ;
+       else playern=1 ;
     }
 
-    printf("\n						Âñå ñïè÷êè âçÿòû. Èãðîê N%d ïîáåäèë!\n",playern) ;
+    printf("\n						All the matches are taken. Player N%d won!\n",playern) ;
 }
 
 int main()
 {
     setlocale(LC_ALL,"Russian");
     
-printf("Íà ïîëíûé ýêðàí(îáðàòíî) alt+enter") ;
-printf("\n\n\n\n		Èãðà '100 ñïè÷åê'") ;
+printf("Full screen (back) alt+enter") ;
+printf("\n\n\n\n		game '100 matches'") ;
     while (1) {
-        printf("\n\n		Ãëàâíîå ìåíþ\n") ;
-        printf("		1 - ïðàâèëà\n		2 - èãðà\n		0 - âûõîä\n") ;
+        printf("\n\n		Main menu\n") ;
+        printf("		1 - Regulation\n		2 - Play\n		0 - exit\n") ;
         int sel = 0 ;
         scanf("%d",&sel) ;
-        if (sel==0) break ; 
+        if (sel==0) 
+            break ; 
         if (sel==1) {
-            printf("	Ïðàâèëà èãðû: Íà ñòîëå 100 ñïè÷åê. Ïî î÷åðåäè èãðîêè áóäóò áðàòüñïè÷êè îò 1 äî 10. Ïðîèãðûâàåò òîò êòî âîçüì¸ò ïîñëåäíþþ ñïè÷êó.\n") ;
+            printf("	Rules of the game: 100 matches on the table. In turn, players will take from 1 to 10 matches. The one who takes the last match loses.\n") ;
         }
         if (sel==2) goGame() ;
     }
