c88608bd0a52358eb210145098a4cc3e80b5e54a
ser2400 <Ser240give@gmail.com>
2018-06-18 03:39:18 +0700

    main.c delete const

--- test/main.c
+++ test/main.c
@@ -14,56 +14,56 @@ CTEST(Input_test, take_1_match_Count_100)
 
 CTEST(Input_test, take_10_matches_Count_100)
 {
-	const int Num = 10;
-	const int Count = 100;
-	const int result = check(Num, Count);
-	const int expected = 1;
-	ASSERT_EQUAL(expected, result);
+    int Num = 10;
+    int Count = 100;
+    int result = check(Num, Count);
+    int expected = 1;
+    ASSERT_EQUAL(expected, result);
 }
 
 CTEST(check_test, take_11_matches_Count_100)
 {
-	const int Num = 11;
-	const int Count = 100;
-	const int result = check(Num, Count);
-	const int expected = 0;
-	ASSERT_EQUAL(expected, result);
+    int Num = 11;
+    int Count = 100;
+    int result = check(Num, Count);
+    int expected = 0;
+    ASSERT_EQUAL(expected, result);
 }
 
 CTEST(check_test, take_100_matches_Count_100)
 {
-	const int Num = 100;
-	const int Count = 100;
-	const int result = check(Num, Count);
-	const int expected = 0;
-	ASSERT_EQUAL(expected, result);
+    int Num = 100;
+    int Count = 100;
+    int result = check(Num, Count);
+    int expected = 0;
+    ASSERT_EQUAL(expected, result);
 }
 
 CTEST(check_test, take_10_matches_Count_8)
 {
-	const int Num = 10;
-	const int Count = 8;
-	const int result = check(Num, Count);
-	const int expected = 0;
-	ASSERT_EQUAL(expected, result);
+    int Num = 10;
+    int Count = 8;
+    int result = check(Num, Count);
+    int expected = 0;
+    ASSERT_EQUAL(expected, result);
 }
 
 CTEST(check_test, take_11_matches_Count_8)
 {
-	const int Num = 11;
-	const int Count = 8;
-	const int result = check(Num, Count);
-	const int expected = 0;
-	ASSERT_EQUAL(expected, result);
+    int Num = 11;
+    int Count = 8;
+    int result = check(Num, Count);
+    int expected = 0;
+    ASSERT_EQUAL(expected, result);
 }
 
 CTEST(check_test, take_1_match_Count_1)
 {
-	const int Num = 1;
-	const int Count = 1;
-	const int result = check(Num, Count);
-	const int expected = 1;
-	ASSERT_EQUAL(expected, result);
+    int Num = 1;
+    int Count = 1;
+    int result = check(Num, Count);
+    int expected = 1;
+    ASSERT_EQUAL(expected, result);
 }
 
 int main(int argc, const char** argv) {
