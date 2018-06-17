e50b4fbefa3e015074060aaa372dac3ae7b439b8
GEORGE0051 <36436394+GEORGE0051@users.noreply.github.com>
2018-06-17 18:53:44 +0700

    Update Base.c

--- src/Base.c
+++ src/Base.c
@@ -3,53 +3,67 @@
 int getStickN(int playern, int stickcount) {
     int n ;
     while (1) {
-        printf("\n\n\n\n		Player N%d, enter the number of matches you take:>",playern) ;
+        printf("\n\n\n\n\t\tPlayer N%d, enter the number of matches you take:>",playern) ;
         fflush(stdin) ;
+        
         scanf("%d",&n) ;
         fflush(stdin) ;
 
-        if (n>stickcount) 
-            printf("		You can't have more than that on the table.!\n") ;
-        else
-        if ((n<1)||(n>10)) 
-            printf("		You need to enter from 1 to 10!\n") ;
-        else
-        return n ;
+	        if (n>stickcount) {
+				printf("\tYou can't have more than that on the table.!\n") ;
+			}
+	        else
+		        if ((n<1)||(n>10)) {
+		            printf("\t\tYou need to enter from 1 to 10!\n") ;
+		        }
+		        else
+		        return n ;
     }
 }
 
 void goGame() {
     int stickcount = 100 ;
     int playern = 1 ;
+    
     while (stickcount>0) {
-       printf("\n\n\n\n							    Current number of matches: %d\n",stickcount) ;
-       int n = getStickN(playern,stickcount);
+       printf("\n\n\n\n\t\t\t\t\t\t\tCurrent number of matches: %d\n",stickcount) ;
+       
+       int n = getStickN(playern,stickcount);{
        stickcount-=n ;
+		}
+		
        if (playern==1) playern=2 ;
        else playern=1 ;
     }
 
-    printf("\n						All the matches are taken. Player N%d won!\n",playern) ;
+    printf("\n\t\t\t\t\tAll the matches are taken. Player N%d won!\n",playern) ;
 }
 
 int main()
 {
-    
-printf("Full screen (back) alt+enter") ;
-printf("\n\n\n\n		Game '100 matches'") ;
+	printf("Full screen (back) alt+enter\n\n\n\n\t\tGame '100 matches'") ;
     while (1) {
-        printf("\n\n		Main menu\n") ;
-        printf("		1 - Regulation\n		2 - Play\n		0 - exit\n") ;
-        int sel = 0 ;
+        printf("\n\n\t\tMain menu\n") ;
+        printf("\t\t1 - Regulation\n\t\t2 - Play\n\t\t0 - exit\n") ;
+        
+        int sel = 0 ;{
         scanf("%d",&sel) ;
-        if (sel==0) 
-            break ; 
-        if (sel==1) {
-            printf("	Rules of the game: 100 matches on the table. In turn, players will take from 1 to 10 matches. The one who takes the last match loses.\n") ;
         }
-        if (sel==2) goGame() ;
+        
+        if (sel==0) {
+            break ; 
+        }
+            
+        if (sel==1) {
+            printf("\tRules of the game: 100 matches on the table.\n") ;
+			printf("\tIn turn, players will take from 1 to 10 matches.\n") ;
+			printf("\tThe one who takes the last match loses.\n") ;
+        }
+        
+        if (sel==2) {
+			goGame() ;
+		}
     }
     return 0 ;
 }
-
 
