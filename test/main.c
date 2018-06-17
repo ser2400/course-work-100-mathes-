5fa30779a9542652c0033151981b5fccce58577a
ser2400 <36228146+ser2400@users.noreply.github.com>
2018-06-17 14:37:02 +0300

    Update main.c

--- test/main.c
+++ test/main.c
@@ -1,8 +1,66 @@
-#define CTEST_MAIN
-
 #include "ctest.h"
+#include "base.c"
 
-int main(int argc, const char** argv)
+
+CTEST(Input_test, take_1_match_Count_100)
 {
-    return ctest_main(argc, argv);
+	const int Num = 2;
+	const int Count = 100;
+	const int result = Input(Num, Count);
+	const int expected = 1;
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(Input_test, take_10_matches_Count_100)
+{
+	const int Num = 10;
+	const int Count = 100;
+	const int result = Input(Num, Count);
+	const int expected = 1;
+	ASSERT_EQUAL(expected, result);
+}
+
+CTEST(Input_test, take_11_matches_Count_100)
+{
+	const int Num = 11;
+	const int Count = 100;
+	const int result = Input(Num, Count);
+	const int expected = 0;
+	ASSERT_EQUAL(expected, result);
+}
+
+CTEST(Input_test, take_100_matches_Count_100)
+{
+	const int Num = 100;
+	const int Count = 100;
+	const int result = Input(Num, Count);
+	const int expected = 0;
+	ASSERT_EQUAL(expected, result);
+}
+
+CTEST(Input_test, take_10_matches_Count_8)
+{
+	const int Num = 10;
+	const int Count = 8;
+	const int result = Input(Num, Count);
+	const int expected = 0;
+	ASSERT_EQUAL(expected, result);
+}
+
+CTEST(Input_test, take_11_matches_Count_8)
+{
+	const int Num = 11;
+	const int Count = 8;
+	const int result = Input(Num, Count);
+	const int expected = 0;
+	ASSERT_EQUAL(expected, result);
+}
+
+CTEST(Input_test, take_1_match_Count_1)
+{
+	const int Num = 1;
+	const int Count = 1;
+	const int result = Input(Num, Count);
+	const int expected = 1;
+	ASSERT_EQUAL(expected, result);
 }
